c37a7c9eb5a3fe494b38259f4c1f352b2f126484
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-07-02 17:57:21 +0300

    Add more logic

--- relayers/packages/wormhole/src/config/env.ts
+++ relayers/packages/wormhole/src/config/env.ts
@@ -1,6 +1,7 @@
 import { Environment } from '@wormhole-foundation/relayer-engine';
 
 export interface Env {
+  APP_NAME: string;
   ENVIRONMENT: Environment;
 
   REDIS_HOST: string;
@@ -8,8 +9,11 @@ export interface Env {
   REDIS_USERNAME: string;
   REDIS_PASSWORD: string;
   REDIS_ATTEMPTS: number;
-  REDIS_NAMESPACE: string;
   REDIS_QUEUE: string;
 
   SPY_URL: string;
+
+  EVM_PRIVATE_KEY: string;
+  SOL_PRIVATE_KEY: string;
+  COSMOS_MNEMONIC: string;
 }
